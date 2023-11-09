f05413ff3e39112fb45a14fd870283aa365dd5c1
declick <et.miracle@hotmail.fr>
2023-11-09 15:23:16 +0100

    easter egg

--- js/script.js
+++ js/script.js
@@ -128,7 +128,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
 /* EFFET WOW*/
 
-    
+/* EASTER EGGE */
+const modal = document.getElementById('videoModal');
+const easterEggTrigger = document.getElementById('easterEggTrigger');
+const closeModal = document.getElementById('closeModal');
+const video = document.getElementById('easterEggVideo');
+
+easterEggTrigger.addEventListener('click', () => {
+  modal.style.display = 'block';
+  video.play();
+});
+
+closeModal.addEventListener('click', () => {
+  modal.style.display = 'none';
+  video.pause();
+  video.currentTime = 0;
+});
+
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+    video.pause();
+    video.currentTime = 0;
+  }
+});  
 
 /* DARK MODE */      
 document.addEventListener('DOMContentLoaded',  () =>{
